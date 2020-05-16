f512bbbb0f61a7d07671f7edc22e067079e3ea26
Jon Rogers <jon@carbonfive.com>
2020-05-15 18:45:21 -0700

    Add different components to see whos fastest

---
+++ app/components/user_view_component.rb
@@ -0,0 +1,17 @@
+class UserViewComponent < ViewComponent::Base
+  attr_accessor :name
+
+  DOMAINS = [
+    "https://www.placecage.com/",
+    "https://placekitten.com/",
+    "https://placebear.com/",
+    "https://www.stevensegallery.com/",
+  ]
+
+  def image
+    width = rand(10) * 10 + 300
+    height = rand(10) * 10 + 200
+    domain = DOMAINS.sample
+    "#{domain}#{width}/#{height}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get "/partials_collection", to: "pages#partials_collection"
+  get "/partials", to: "pages#partials"
+  get "/view_components", to: "pages#view_components"
+  get "/rcode5_collection", to: "pages#rcode5_collection"
   root to: "pages#root"
 end
