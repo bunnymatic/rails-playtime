91facd7c21d7aea07a4e1d5382b58a6b7c376aec
Jon Rogers <jon@carbonfive.com>
2020-05-15 23:38:16 -0700

    Revamp so the partials are rendering almost nothing

--- app/components/user_component.rb
+++ app/components/user_component.rb
@@ -1,17 +1,2 @@
 class UserComponent < Rcode5::Component
-  attr_accessor :name
-
-  DOMAINS = [
-    "https://www.placecage.com/",
-    "https://placekitten.com/",
-    "https://placebear.com/",
-    "https://www.stevensegallery.com/",
-  ]
-  
-  def image
-    width = rand(10) * 10 + 300
-    height = rand(10) * 10 + 200
-    domain = DOMAINS.sample
-    "#{domain}#{width}/#{height}"
-  end
 end
