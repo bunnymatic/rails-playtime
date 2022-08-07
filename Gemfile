ea02c5e486923a2ece28d960f032098c0c4d8337
Jon Rogers <jon@carbonfive.com>
2022-08-06 18:48:36 -0700

    Bump to rails 6

--- Gemfile
+++ Gemfile
@@ -5,12 +5,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "2.6.6"
 
 # Back-endy
-gem "bootsnap", require: false
 gem "faker"
 gem "pg"
 gem "puma"
 gem "rack-canonical-host"
-gem "rails", "~> 5.2.0"
+gem "rails", "~> 6.x"
+gem 'listen'
 
 # Front-endy
 gem "autoprefixer-rails"
@@ -19,18 +19,9 @@ gem "coffee-rails"
 gem "jquery-rails"
 gem "sass-rails", require: false # Only needed for generator (e.g. rail g controller Users)
 gem "sassc-rails"
+gem "sprockets-rails", require: 'sprockets/railtie'
 gem "simple_form"
 gem "slim-rails"
-gem "uglifier"
-
-# Tools
-gem "awesome_print"
-
-# Env specific dependencies...
-
-group :production, :acceptance do
-  gem "rack-timeout"
-end
 
 group :development, :test do
   gem "factory_bot_rails"
@@ -42,24 +33,8 @@ group :development, :test do
   gem "timecop"
 end
 
-group :development do
-  gem "annotate"
-  gem "better_errors"
-  gem "binding_of_caller"
-  gem "dotenv-rails"
-  gem "launchy"
-  gem "listen"
-  gem "spring"
-  gem "spring-commands-rspec"
-  gem "spring-watcher-listen"
-  # gem "guard"
-  # gem "guard-rspec", ">= 4.6.5" # Resolves to 1.x without a version constraint. :/
-  # gem "guard-livereload"
-end
-
 group :test do
   gem "capybara"
-  # gem "capybara-email"
   gem "capybara-selenium"
   gem "chromedriver-helper"
   gem "simplecov"
--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,14 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+#
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -15,43 +21,23 @@ port ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
 # Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
+# Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers_count = ENV.fetch("WEB_CONCURRENCY") { 1 }.to_i
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-if workers_count > 1
-  workers workers_count
-
-  # Use the `preload_app!` method when specifying a `workers` number.
-  # This directive tells Puma to first boot the application and load code
-  # before forking the application. This takes advantage of Copy On Write
-  # process behavior so workers use less memory.
-  #
-  preload_app!
-
-  # The code in the `on_worker_boot` will be called if you are using
-  # clustered mode by specifying a number of `workers`. After each worker
-  # process is booted this block will be run, if you are using `preload_app!`
-  # option you will want to use this block to reconnect to any threads
-  # or connections that may have been created at application boot, Ruby
-  # cannot share connections between processes.
-  #
-  on_worker_boot do
-    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-  end
-
-  # If you are preloading your application and using ActiveRecord, it's
-  # recommended that you close any connections to the database before workers
-  # are forked to prevent connection leakage.
-  #
-  before_fork do
-    ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
-  end
-end
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+# preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
